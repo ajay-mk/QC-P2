a3b0f839fc2df1bbac85761a712fabd339c07304
Ajay <ajaymk22@vt.edu>
2022-12-05 21:18:00 -0500

    cc source and header files added

---
+++ cc.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ajay Melekamburath on 12/5/22.
+//
+
+#include "cc.h"
---
+++ cc.h
@@ -0,0 +1,13 @@
+//
+// Created by Ajay Melekamburath on 12/5/22.
+//
+
+#ifndef P2_CC_H
+#define P2_CC_H
+
+#include "general.h"
+#include "integrals.h"
+#include "hf.h"
+#include "mp2.h"
+
+#endif//P2_CC_H
